3e3ae273f4a1f304ef9c63bf7196252b3b806f50
404SegFault <daniel062588@gmail.com>
2022-05-17 13:18:59 +1000

    Update test.py

--- Major_TestSEndCommand/test.py
+++ Major_TestSEndCommand/test.py
@@ -1,10 +1,15 @@
 import serial
 
-serialPort = serial.Serial(port="COM4", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
-while(1):
-    
-    if(serialPort.in_waiting > 0):
-        serialPort.write(str.encode('l 20'))
-        serialPort.flush()
-        print(serialPort.in_waiting)
+def serialWrite(com_port):
+    serialPort = serial.Serial(port=com_port, baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+
+    while(1):
+        if(serialPort.in_waiting > 0):
+            serialPort.write(str.encode("l 20"))
+            serialPort.flush()
+            print(serialPort.in_waiting)
+
+
+if __name__ == '__main__':
+    serialWrite("COM4")
