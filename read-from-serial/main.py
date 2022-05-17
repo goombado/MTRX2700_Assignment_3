22ff678efe3abdddac2330402e14f927d11c46cd
Andrei <andreiagnew@hotmail.com>
2022-05-17 14:21:37 +1000

    added python read serial code

---
+++ read-from-serial/main.py
@@ -0,0 +1,85 @@
+# This is a Python script to parse the example messages from a file.
+
+import time
+import serial
+import struct
+import traceback
+
+MSG_HEADER_SIZE = 16
+
+def read_packet(f, outFile = None):
+    header_bytes = f.readline()
+    if outFile is not None:
+        outFile.write(header_bytes)
+    else:
+        print(header_bytes)
+    return True
+
+    if len(header_bytes) < MSG_HEADER_SIZE:
+        # must be out of messages
+        return False
+
+    header_data = struct.unpack(">H8sHHH", header_bytes)
+    print(header_data)
+    print("header sentinels: " + str(hex(header_data[0])) + ", " + str(hex(header_data[4])))
+
+    print(header_data[1])
+    message_type = header_data[1].split(b'\0', 1)[0]  # remove the null characters from the string
+    # print(message_type)
+    print("message size: " + str(header_data[2]))
+
+    if message_type == b"text":
+        text_bytes = f.read(header_data[2])
+        print("text message: " + str(text_bytes))
+    elif message_type == b"gyro":
+        gyro_bytes = f.read(header_data[2])
+        gyro_data = struct.unpack(">hhhhH", gyro_bytes)
+        print("gyro message: " + str(gyro_data[1]) + ", " + str(gyro_data[2]) + ", " + str(gyro_data[3]) + ", time=" + str(gyro_data[4]))
+    elif message_type == b"buttons":
+        buttons_bytes = f.read(header_data[2])
+        print("buttons message: " + str(hex(buttons_bytes[1])) + ", time=" + str(buttons_bytes[2]))
+
+    return True
+
+
+def read_file(file_name):
+    # open the file
+    with open(file_name, "r") as f:
+        while True:
+            try:
+                if not read_packet(f):
+                    break
+            except Exception as e:
+                print(traceback.format_exc())
+                break
+                # Logs the error appropriately. 
+    
+
+def read_serial(com_port):
+    serialPort = serial.Serial(port=com_port, baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+    serialString = ""  # Used to hold data coming over UART
+
+    outFile = open('wall_data.csv', 'w')
+    while True:
+
+        # Wait until there is data waiting in the serial buffer
+        if serialPort.in_waiting > 0:
+
+            try:
+                if not read_packet(serialPort, outFile=outFile):
+                    outFile.close()
+                    break
+            except Exception as e:
+                # Logs the error appropriately. 
+                print(traceback.format_exc())
+                break
+       
+        else:
+            time.sleep(0.05)
+
+
+
+# main program entry point
+if __name__ == '__main__':
+    #read_file('C:/Users/Stewart Worrall/Documents/data/test.hex')
+    read_serial("COM6")
