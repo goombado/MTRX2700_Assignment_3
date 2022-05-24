f9fa9c1093e0cddcc971747cf9f0af392d71a01b
Andrei <andreiagnew@hotmail.com>
2022-05-24 15:58:42 +1000

    formatting

--- Object_Data_Collection_Real/Object_Data_Collection_Real/Sources/simple_serial.c
+++ Object_Data_Collection_Real/Object_Data_Collection_Real/Sources/simple_serial.c
@@ -87,7 +87,9 @@ void serial_read (void) {
         while(!(SCI1SR1 & 0x80));
         
         // *(SCI1.ControlRegister2) &= ~(SCI1CR2_RIE_MASK);
-        beginScan();
+        if (buffer[0] == 's') {
+            beginScan();
+        }
         // *(SCI1.ControlRegister2) |= SCI1CR2_RIE_MASK;
         
         // Reset buffer
